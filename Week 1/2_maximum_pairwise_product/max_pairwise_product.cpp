6879df048c8e67e059edde585010489c5a61cf8a
mina47 <67565429+mina47@users.noreply.github.com>
2022-02-04 13:03:15 +0200

    Add files via upload

---
+++ Week 1/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std ;
+long long  MaxPairwiseProduct(vector<int> numbers) {
+     int n =numbers.size();
+     sort (numbers.begin() ,numbers.end());
+     return ((long long )(numbers[n-1]))* numbers[n-2];
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> numbers[i];
+    }
+
+    std::cout << MaxPairwiseProduct(numbers) << '\n';
+    return 0;
+}
